3849cb2d7e32121ac8bd88ba86ae46b238cce30d
Peter Williams <pwil3058@gmail.com>
2019-11-01 16:02:32 +1000

    Framewor for deriving pw_gix::Wrapper
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- apaint_gtk_boilerplate/src/lib.rs
+++ apaint_gtk_boilerplate/src/lib.rs
@@ -1,7 +1,25 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+// Copyright 2019 Peter Williams <pwil3058@gmail.com> <pwil3058@bigpond.net.au>
+
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+
+#[proc_macro_derive(Wrapper)]
+pub fn wrapper_derive(input: TokenStream) -> TokenStream {
+    let parsed_input: syn::DeriveInput = syn::parse_macro_input!(input);
+    let struct_name = parsed_input.ident;
+    let _name = struct_name.to_string();
+    let (impl_generics, ty_generics, where_clause) = parsed_input.generics.split_for_impl();
+
+    let tokens = quote! {
+        impl #impl_generics PackableWidgetObject for #struct_name #ty_generics #where_clause {
+            type PWT = whatever;
+
+            fn pwo(&self) -> Self::PWT {
+                self.first.clone()
+            }
+        }
+    };
+    proc_macro::TokenStream::from(tokens)
 }
